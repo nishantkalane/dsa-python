c5cb7cb0bc556584c6a99539ba96fda40b54e671
nishantkalane <nishantnkalane@gmail.com>
2026-09-08 11:03:50 +0530

    completed 8 sep revision problems

--- 01_Basics/03_basic_maths/Revision_01/Revision_04_Pallindrome_Number.py
+++ 01_Basics/03_basic_maths/Revision_01/Revision_04_Pallindrome_Number.py
@@ -2,11 +2,20 @@
 class Solution:
 
     def solve(self,n):
-        print()
-
+        dup=n
+        rev_no =0
+        while n >0:
+            last_digit= n%10
+            rev_no=(rev_no*10)+last_digit
+            n =n//10
+        if rev_no == dup:
+            return True
+        else:
+            return False
 if __name__ == "__main__":
     solution = Solution()
 
     # Test your solution
-    inp= 36
+    inp= 123
     result=solution.solve(inp)
+    print(result)
--- 01_Basics/03_basic_maths/Revision_01/Revision_05_Armstrong_Number.py
+++ 01_Basics/03_basic_maths/Revision_01/Revision_05_Armstrong_Number.py
@@ -2,11 +2,22 @@
 class Solution:
 
     def solve(self,n):
-        print()
+        dup=n
+        length=len(str(n))
+        armstrong_number=0
+        while n > 0:
+            last_digit=n%10
+            armstrong_number += last_digit**length
+            n //=10
+        if armstrong_number ==dup:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     solution = Solution()
 
     # Test your solution
-    inp= 36
+    inp= 32
     result=solution.solve(inp)
+    print(result)
